be43816dceb52af8c600234aca1789029d767b21
S1monG <si5564gu-s@student.lu.se>
2022-05-11 23:25:34 +0200

    ksd

--- src/window.rs
+++ src/window.rs
@@ -36,57 +36,8 @@ pub fn start() {
     // while loop is needed or else the window closes when the lifetime of the window ends, aka this function ends
     while let Some(event) = window.next() {
 
-
-
-        // draw the board
-        window.draw_2d(&event, |context, graphics, device| {
-            clear([1.0; 4], graphics);
-
-            let rectangle = Rectangle::new_round(color::BLACK, 5.0);
-
-            rectangle.draw_from_to([SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
-                &draw_state::DrawState::default(),
-                context.transform,
-                graphics,
-            );
-            rectangle.draw_from_to(  [2.0 * SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [2.0 * SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
-                &draw_state::DrawState::default(),
-                context.transform,
-                graphics,
-            );
-            rectangle.draw_from_to([0.0 + MARGIN, SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, SIZE / 3.0 + MARGIN / 2.0],
-                &draw_state::DrawState::default(),
-                context.transform,
-                graphics,
-            );
-            rectangle.draw_from_to([0.0 + MARGIN, 2.0 * SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, 2.0 * SIZE / 3.0 + MARGIN / 2.0],
-                &draw_state::DrawState::default(),
-                context.transform,
-                graphics,
-            );
-
-            let mut text = "";
-            for (i, s) in b.board.iter().enumerate() {
-                if *s == -1 {
-                    text = "O";
-                } else if *s == 1 {
-                    text = "X";
-                } else {
-                    continue;
-                }
-                text::Text::new_color(color::BLACK, FONT_SIZE as u32).draw(
-                    text,
-                    &mut glyphs,
-                    &context.draw_state,
-                    context.transform.trans(BOX_PLACEMENT[i][1], BOX_PLACEMENT[i][0]), 
-                    graphics
-                ).unwrap();
-
-            }
-        });
-
-        // save the last variable of the mouse curser in a variable and update it when the new one isn't None
-        if let Some(pos) = event.mouse_cursor_args() {
+         // save the last variable of the mouse curser in a variable and update it when the new one isn't None
+         if let Some(pos) = event.mouse_cursor_args() {
             last_pos = pos;
         }
         // update the board with player move
@@ -127,8 +78,54 @@ pub fn start() {
         // update board with computer move if it is the computers turn
         if b.turn() == -1 {
             b.update(-1, tictactoe::computer::random::random_move(&b));
-            b.print_board();
         }
 
+        // draw the board
+        window.draw_2d(&event, |context, graphics, device| {
+            clear([1.0; 4], graphics);
+
+            let rectangle = Rectangle::new_round(color::BLACK, 5.0);
+
+            rectangle.draw_from_to([SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
+                &draw_state::DrawState::default(),
+                context.transform,
+                graphics,
+            );
+            rectangle.draw_from_to(  [2.0 * SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [2.0 * SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
+                &draw_state::DrawState::default(),
+                context.transform,
+                graphics,
+            );
+            rectangle.draw_from_to([0.0 + MARGIN, SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, SIZE / 3.0 + MARGIN / 2.0],
+                &draw_state::DrawState::default(),
+                context.transform,
+                graphics,
+            );
+            rectangle.draw_from_to([0.0 + MARGIN, 2.0 * SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, 2.0 * SIZE / 3.0 + MARGIN / 2.0],
+                &draw_state::DrawState::default(),
+                context.transform,
+                graphics,
+            );
+
+            for (i, s) in b.board.iter().enumerate() {
+                if *s == -1 {
+                    text::Text::new_color(color::BLACK, FONT_SIZE as u32).draw(
+                        "O",
+                        &mut glyphs,
+                        &context.draw_state,
+                        context.transform.trans(BOX_PLACEMENT[i][1], BOX_PLACEMENT[i][0]), 
+                        graphics
+                    ).unwrap();
+                } else if *s == 1 {
+                    text::Text::new_color(color::BLACK, FONT_SIZE as u32).draw(
+                        "X",
+                        &mut glyphs,
+                        &context.draw_state,
+                        context.transform.trans(BOX_PLACEMENT[i][1], BOX_PLACEMENT[i][0]), 
+                        graphics
+                    ).unwrap();
+                }
+            }
+        });
     }
 }
