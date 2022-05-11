b1dafe8f9bce17a0e30ee8ea6a0c05c1e67d699b
S1monG <si5564gu-s@student.lu.se>
2022-05-11 22:56:30 +0200

    .

--- src/computer/random.rs
+++ src/computer/random.rs
@@ -1,7 +1,16 @@
 // Picks random place for tictactoe
 
 use crate::Board;
+use rand::seq::SliceRandom;
 
-pub fn random_move(board: &Board, turn: isize) -> usize {
-    todo!()
+pub fn random_move(b: &Board) -> usize {
+    let mut empty_boxes: Vec<usize> = Vec::new();
+    for (i, t) in b.board.iter().enumerate() {
+        if *t == 0 {
+            empty_boxes.push(i);
+        }
+    }
+
+    *empty_boxes.choose(&mut rand::thread_rng())
+        .expect("No empty places left")
 }
--- src/window.rs
+++ src/window.rs
@@ -35,36 +35,31 @@ pub fn start() {
 
     // while loop is needed or else the window closes when the lifetime of the window ends, aka this function ends
     while let Some(event) = window.next() {
+
+
+
         // draw the board
         window.draw_2d(&event, |context, graphics, device| {
             clear([1.0; 4], graphics);
 
             let rectangle = Rectangle::new_round(color::BLACK, 5.0);
 
-            rectangle.draw_from_to(
-                [SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN],
-                [SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
+            rectangle.draw_from_to([SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
                 &draw_state::DrawState::default(),
                 context.transform,
                 graphics,
             );
-            rectangle.draw_from_to(
-                [2.0 * SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN],
-                [2.0 * SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
+            rectangle.draw_from_to(  [2.0 * SIZE / 3.0 - MARGIN / 2.0, 0.0 + MARGIN], [2.0 * SIZE / 3.0 + MARGIN / 2.0, SIZE - MARGIN],
                 &draw_state::DrawState::default(),
                 context.transform,
                 graphics,
             );
-            rectangle.draw_from_to(
-                [0.0 + MARGIN, SIZE / 3.0 - MARGIN / 2.0],
-                [SIZE - MARGIN, SIZE / 3.0 + MARGIN / 2.0],
+            rectangle.draw_from_to([0.0 + MARGIN, SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, SIZE / 3.0 + MARGIN / 2.0],
                 &draw_state::DrawState::default(),
                 context.transform,
                 graphics,
             );
-            rectangle.draw_from_to(
-                [0.0 + MARGIN, 2.0 * SIZE / 3.0 - MARGIN / 2.0],
-                [SIZE - MARGIN, 2.0 * SIZE / 3.0 + MARGIN / 2.0],
+            rectangle.draw_from_to([0.0 + MARGIN, 2.0 * SIZE / 3.0 - MARGIN / 2.0], [SIZE - MARGIN, 2.0 * SIZE / 3.0 + MARGIN / 2.0],
                 &draw_state::DrawState::default(),
                 context.transform,
                 graphics,
@@ -101,38 +96,39 @@ pub fn start() {
 
                 if x < SIZE / 3.0 {
                     if y < SIZE / 3.0 {
-                        b.update(1, 0)
+                        b.update(1, 0);
                     } else if y < 2.0 * SIZE / 3.0 {
-                        b.update(1, 1)
+                        b.update(1, 1);
                     } else {
-                        b.update(1, 2)
+                        b.update(1, 2);
                     }
                 } else if x < 2.0 * SIZE / 3.0 {
                     if y < SIZE / 3.0 {
-                        b.update(1, 3)
+                        b.update(1, 3);
                     } else if y < 2.0 * SIZE / 3.0 {
-                        b.update(1, 4)
+                        b.update(1, 4);
                     } else {
-                        b.update(1, 5)
+                        b.update(1, 5);
                     }
                 } else {
                     if y < SIZE / 3.0 {
-                        b.update(1, 6)
+                        b.update(1, 6);
                     } else if y < 2.0 * SIZE / 3.0 {
-                        b.update(1, 7)
+                        b.update(1, 7);
                     } else {
-                        b.update(1, 8)
+                        b.update(1, 8);
                     }
                 }
             } else if input == Button::Keyboard(Key::Q) {
                 b.clear();
-                continue;
             }
         };
 
         // update board with computer move if it is the computers turn
         if b.turn() == -1 {
-            b.update(-1, tictactoe::computer::random::random_move(&b, -1))
+            b.update(-1, tictactoe::computer::random::random_move(&b));
+            b.print_board();
         }
+
     }
 }
