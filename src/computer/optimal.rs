9c17a69216b3de01d19c7504a32288842e7b304a
S1monG <si5564gu-s@student.lu.se>
2022-05-17 16:22:32 +0200

    almost done with gui

--- src/computer/optimal.rs
+++ src/computer/optimal.rs
@@ -1,6 +1,6 @@
 // calculates the optimal place for tictactoe using the minimax algorithm
 use crate::Board;
 
-pub fn best_move(board: &Board, turn: isize) -> usize {
+pub fn optimal_move(board: &Board, turn: isize) -> usize {
     todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,112 @@
-use tictactoe::Board;
-
-const width: u32 = 500;
-const height: u32 = 500;
-
 use macroquad::prelude::*;
+use tictactoe::Board;
+use tictactoe::computer::{random::random_move, optimal::optimal_move};
 
-#[macroquad::main("Life")]
+const WIDTH: f32 = 500.;
+const HEIGHT: f32 = 500.;
+const THICKNESS: f32 = 10.;
+const FONT_SIZE: f32 = 100.;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: String::from("Tic Tac Toe"),
+        window_width: WIDTH as i32,
+        window_height: HEIGHT as i32,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
 
-    println!("{}        {}", w, h);
-
-    let mut image = Image::gen_image_color(width as u16, height as u16, WHITE);
-
-    let texture = Texture2D::from_image(&image);
+    let mut b: Board = Board::new();
 
     loop {
-        clear_background(WHITE);
-
-        let w = image.width();
-        let h = image.height();
 
         // update state ...
+        if is_key_pressed(KeyCode::Q) || is_key_pressed(KeyCode::Escape) {
+            println!("Bye for know!");
+            std::process::exit(0);
+        }
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (x, y) = mouse_position();
+            let index = get_box(x, y);
+            if b.board[index] == 0 {
+                b.update(1, index);
+            }
+        }
+
+        if b.turn() == -1 {
+            b.update(-1, random_move(&b));
+        }
 
 
         //draw state
+        clear_background(WHITE);
+        draw_line(WIDTH / 3., 0., WIDTH / 3., HEIGHT, THICKNESS, BLACK);
+        draw_line(2. * WIDTH / 3., 0., 2. * WIDTH / 3., HEIGHT, THICKNESS, BLACK);
+        draw_line(0., HEIGHT / 3., WIDTH, HEIGHT / 3., THICKNESS, BLACK);
+        draw_line(0., 2. * HEIGHT / 3., WIDTH, 2. * HEIGHT / 3., THICKNESS, BLACK);
 
-        for i in 0..buffer.len() {
-            cells[i] = buffer[i];
-
-            image.set_pixel(
-                (i % w) as u32,
-                (i / w) as u32,
-                match buffer[i as usize] {
-                    CellState::Alive => BLACK,
-                    CellState::Dead => WHITE,
-                },
-            );
+        for i in 0..3 {
+            for j in 0..3 {
+                let symbol = b.board[j * 3 + i];
+                if symbol == 1 {
+                    draw_text(
+                        "X",
+                        i as f32 * WIDTH / 3. + FONT_SIZE / 2.,
+                        j as f32 * WIDTH / 3. + FONT_SIZE,
+                        FONT_SIZE,
+                        BLACK,
+                    );
+                } else if symbol == -1 {
+                    draw_text(
+                        "O",
+                        i as f32 * WIDTH / 3. + FONT_SIZE / 2.,
+                        j as f32 * WIDTH / 3. + FONT_SIZE,
+                        FONT_SIZE,
+                        BLACK,
+                    );
+                }
+            }
         }
 
-        texture.update(&image);
-
-        draw_texture(texture, 0., 0., WHITE);
+        //check win
+        match b.is_win() {
+            1 => draw_text("You Win!", WIDTH/2., HEIGHT/2., FONT_SIZE*3., BLACK),
+            -1 => draw_text("Computer Win!", WIDTH/2., HEIGHT/2., FONT_SIZE*3., BLACK),
+            _ => (),
+        }
 
         next_frame().await;
+    }
+}
+
+fn get_box(x: f32, y: f32) -> usize {
+    if y < HEIGHT/3. {
+        if x < WIDTH/3. {
+            0
+        } else if x < 2.*WIDTH/3. {
+            1
+        } else {
+            2
+        }
+    } else if y < 2.*HEIGHT/3. {
+        if x < WIDTH/3. {
+            3
+        } else if x < 2.*WIDTH/3. {
+            4
+        } else {
+            5
+        }
+    } else {
+        if x < WIDTH/3. {
+            6
+        } else if x < 2.*WIDTH/3. {
+            7
+        } else {
+            8
+        }
     }
 }
