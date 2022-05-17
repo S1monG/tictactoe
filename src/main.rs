f510669ddcda2783a606febe1dfddc265dbbfa8d
S1monG <si5564gu-s@student.lu.se>
2022-05-17 12:18:00 +0200

    aeofsncl

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,17 @@
 use tictactoe::Board;
 
+const width: u32 = 500;
+const height: u32 = 500;
 
 use macroquad::prelude::*;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum CellState {
-    Alive,
-    Dead,
-}
-
 #[macroquad::main("Life")]
 async fn main() {
-    let w = screen_width() as usize;
-    let h = screen_height() as usize;
 
     println!("{}        {}", w, h);
 
-    let mut cells = vec![CellState::Dead; w * h];
-    let mut buffer = vec![CellState::Dead; w * h];
+    let mut image = Image::gen_image_color(width as u16, height as u16, WHITE);
 
-    let mut image = Image::gen_image_color(w as u16, h as u16, WHITE);
-
-    for y in 0..h {
-        for x in 0..w {
-            if rand::gen_range(0, 5) == 0 {
-                cells[y * w + x] = CellState::Alive;
-            }
-        }
-    }
     let texture = Texture2D::from_image(&image);
 
     loop {
@@ -36,47 +20,10 @@ async fn main() {
         let w = image.width();
         let h = image.height();
 
-        for y in 0..h as i32 {
-            for x in 0..w as i32 {
-                let mut neighbors_count = 0;
+        // update state ...
 
-                for j in -1i32..=1 {
-                    for i in -1i32..=1 {
-                        // out of bounds
-                        if y + j < 0 || y + j >= h as i32 || x + i < 0 || x + i >= w as i32 {
-                            continue;
-                        }
-                        // cell itself
-                        if i == 0 && j == 0 {
-                            continue;
-                        }
 
-                        let neighbor = cells[(y + j) as usize * w + (x + i) as usize];
-                        if neighbor == CellState::Alive {
-                            neighbors_count += 1;
-                        }
-                    }
-                }
-
-                let current_cell = cells[y as usize * w + x as usize];
-                buffer[y as usize * w + x as usize] = match (current_cell, neighbors_count) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (CellState::Alive, x) if x < 2 => CellState::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (CellState::Alive, 2) | (CellState::Alive, 3) => CellState::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (CellState::Alive, x) if x > 3 => CellState::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (CellState::Dead, 3) => CellState::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-            }
-        }
+        //draw state
 
         for i in 0..buffer.len() {
             cells[i] = buffer[i];
@@ -95,6 +42,6 @@ async fn main() {
 
         draw_texture(texture, 0., 0., WHITE);
 
-        next_frame().await
+        next_frame().await;
     }
 }
